0c71cdca1cac8585cbc1936b26b6bac282be86ea
yingchi <yingchi.pei@gmail.com>
2017-08-25 08:42:25 +0800

    update alias

--- setup/aws-alias.sh
+++ setup/aws-alias.sh
@@ -1,3 +1,16 @@
+export instanceId=i-00e5a50e782500688
+export subnetId=subnet-6ddebf36
+export securityGroupId=sg-2d84a357
+export instanceUrl=ec2-52-33-25-250.us-west-2.compute.amazonaws.com
+export routeTableId=rtb-fe2a3398
+export name=fast-ai
+export vpcId=vpc-d0880fb6
+export internetGatewayId=igw-57583530
+export subnetId=subnet-6ddebf36
+export allocAddr=eipalloc-fc9c53c1
+export assocId=eipassoc-34a22c08
+export routeTableAssoc=rtbassoc-80c312f8
+
 alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
 alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
 alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
@@ -24,5 +37,3 @@ then
     # This is Mac.  Use open to open the notebook
     alias aws-nb='open http://$instanceIp:8888'
 fi
-
-export instanceId=i-07e43b9dccabdcc0c
